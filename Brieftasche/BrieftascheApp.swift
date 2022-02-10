9cb9c931ac9b452d1eaf0e4bf9b05f13faa8c509
Lloyd <johnlloydachondo9@gmail.com>
2022-02-10 21:46:22 +0800

    Initial Commit

---
+++ Brieftasche/BrieftascheApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrieftascheApp.swift
+//  Brieftasche
+//
+//  Created by Lloyd on 2/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BrieftascheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brieftasche/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Brieftasche
+//
+//  Created by Lloyd on 2/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
